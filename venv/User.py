4997683ce30e6abca4b317a98bfdf6ea209c67b1
lero1312 <ma13121999@gmail.com>
2019-04-20 23:52:41 +0200

    User class

---
+++ venv/User.py
@@ -0,0 +1,13 @@
+class User:
+    name = ""
+    phone = ""
+    id = 0
+    state = ""
+    lastVis = ""
+
+    def __init__(self , _name , _phone , _id , _state , _lastVis):
+        name = _name
+        phone = _phone
+        id = _id
+        state = _state
+        lastVis = _lastVis
